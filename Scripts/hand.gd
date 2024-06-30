d7bb39bf412754e674ad63d47c572e8b1c224950
DaniDSan <98471241+DaniDSan@users.noreply.github.com>
2024-06-30 21:31:30 +0200

    Construccion de torres hecha

--- Scripts/hand.gd
+++ Scripts/hand.gd
@@ -6,27 +6,32 @@ var holded_object
 var tile_map: TileMap
 var filled_tiles = []
 
+@export var tower: PackedScene
+
 func _ready():
 	tile_map = get_parent().get_parent().get_node("TileMap")
 	
 func _process(delta):
-	_grab_object()
-	_place_object()
-
-func _on_body_entered(body):
+	grab_object()
+	place_object()
+	
+func _on_area_entered(area):
 	if !is_empty:
 		return
 	is_empty = false
-	holded_object = body
+	holded_object = area
 
-func _grab_object():
+func grab_object():
 	if !is_empty:
 		holded_object.global_position = global_position
 		
-func _place_object():
+func place_object():
 	if !is_empty:
 		var map_pos = tile_map.map_to_local(tile_map.local_to_map(global_position))
 		if Input.is_action_just_pressed("interactuar") && !filled_tiles.has(map_pos):
-			holded_object.global_position = map_pos
+			holded_object.queue_free()
+			var temp_tower = tower.instantiate()
+			find_parent("Main").add_child(temp_tower)
+			temp_tower.global_position = map_pos
 			filled_tiles.append(map_pos)
 			is_empty = true
--- Scripts/objeto_tronco.gd
+++ Scripts/objeto_tronco.gd
@@ -1,7 +1,8 @@
-extends RigidBody2D
+extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pass
 	$AnimacionTronco.play("Spawn")
 	$TroncoSpawn.play()
 
